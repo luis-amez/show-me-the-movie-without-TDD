300101ad7dea35724a092e47740ff6ffd347608e
Luis Amez <luis.amez.vitoria@gmail.com>
2018-04-13 13:33:53 +0200

    Add migration for description and released_on fields on Movie

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180413094021) do
+ActiveRecord::Schema.define(version: 20180413110844) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20180413094021) do
     t.decimal  "total_gross"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.text     "description"
+    t.date     "released_on"
   end
 
 end
